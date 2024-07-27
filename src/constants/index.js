15fe330d849df84e9c2296df56a8477604fe64eb
Harsha <123433288+KesaraBandara@users.noreply.github.com>
2024-07-28 01:52:54 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,57 @@
+
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am an undergraduate student at Cardiff Metropolitan University pursuing a degree in Software engineering. I am currently in the 3rd year, 2nd semester of my program. I am a responsible and hardworking individual, and I believe that I am a good team player. I am looking for a challenging role in a respectable firm where I can put my technical, database, and management talents to good use while also learning about new and developing trends in the IT industry.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Software Engineer with a passion for creating efficient and user-friendly web applications and mobile applications. I have worked with various technologies, including React, ReactNative, Node.js, Spring Boot, MySQL, and PostgreSQL. My journey in web and mobile development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    id: 1,
+    image: project1,
+    link: "https://www.youtube.com/embed/t_C2yEtqFVM",
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "Vue.js", "Spring Boot", "MySQL"],
+  },
+  {
+    title: "Coffee Shop App",
+    id: 2,
+    image: project2,
+    link: "https://www.youtube.com/embed/_63mEm3AMSY",
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    id: 3,
+    image: project3,
+    link: "https://www.youtube.com/embed/t_C2yEtqFVM",
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    id: 4,
+    image: project4,
+    link: "https://www.youtube.com/embed/_63mEm3AMSY",
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+
+export const CONTACT = {
+  address: "C/3 Narangoda Pitagaldeniya, Kegalle, Sri Lanka",
+  phoneNo: "+94717352301",
+  email: "kesrabandara1998@gmail.com",
+};
